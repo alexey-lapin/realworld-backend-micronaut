4ce28e8400f684e7a3c41fd0dfdb86807e20ea25
Alexey Lapin <alexey-lapin@protonmail.com>
2021-02-24 22:03:50 +0300

    bump versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,13 +1,13 @@
 object Versions {
 
     const val lombok = "1.18.18"
-    const val mn = "2.3.1"
+    const val mn = "2.3.3"
     const val jwt = "0.11.2"
     const val logback = "1.2.3"
     const val liquibase = "4.2.2"
 
     const val assertj = "3.19.0"
-    const val mockito = "3.7.7"
+    const val mockito = "3.8.0"
 
     const val jacoco = "0.8.5"
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        id("com.diffplug.spotless") version "5.9.0"
+        id("com.diffplug.spotless") version "5.10.2"
         id("com.github.ben-manes.versions") version "0.36.0"
         id("com.github.johnrengelman.shadow") version "6.1.0"
     }
