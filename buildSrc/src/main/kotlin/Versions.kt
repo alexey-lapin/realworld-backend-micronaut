3f7c1c2a871850446c65da917f4fff88caa449bc
Alexey Lapin <alexey-lapin@protonmail.com>
2021-04-07 21:28:12 +0300

    bump versions (#30)

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,6 +1,6 @@
 object Versions {
 
-    const val lombok = "1.18.18"
+    const val lombok = "1.18.20"
     const val mn = "2.4.2"
     const val jwt = "0.11.2"
     const val logback = "1.2.3"
@@ -8,7 +8,7 @@ object Versions {
     const val slf4j = "1.7.30"
 
     const val assertj = "3.19.0"
-    const val mockito = "3.8.0"
+    const val mockito = "3.9.0"
 
     const val jacoco = "0.8.5"
 
