8019abd4afe6cf915b8fa99adcf5de4f33cddf4b
Alexey Lapin <alexey-lapin@protonmail.com>
2021-06-14 16:58:57 +0300

    bump versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,14 +1,14 @@
 object Versions {
 
     const val lombok = "1.18.20"
-    const val mn = "2.5.4"
+    const val mn = "2.5.5"
     const val jwt = "0.11.2"
     const val logback = "1.2.3"
     const val liquibase = "4.2.2"
     const val slf4j = "1.7.30"
 
     const val assertj = "3.19.0"
-    const val mockito = "3.10.0"
+    const val mockito = "3.11.1"
 
     const val jacoco = "0.8.5"
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
     plugins {
-        id("com.diffplug.spotless") version "5.12.5"
+        id("com.diffplug.spotless") version "5.13.0"
         id("com.github.ben-manes.versions") version "0.39.0"
         id("com.github.johnrengelman.shadow") version "7.0.0"
         id("com.gorylenko.gradle-git-properties") version "2.2.4"
-        id("io.micronaut.application") version "1.5.0"
-        id("io.micronaut.library") version "1.5.0"
+        id("io.micronaut.application") version "1.5.1"
+        id("io.micronaut.library") version "1.5.1"
     }
 }
 
