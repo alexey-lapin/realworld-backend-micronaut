d2fe6360d7cd2a7a3eff6bd6b0640c3240f08464
Alexey Lapin <alexey-lapin@protonmail.com>
2021-07-06 21:40:17 +0300

    bump mn version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 
     const val lombok = "1.18.20"
-    const val mn = "2.5.6"
+    const val mn = "2.5.8"
     const val jwt = "0.11.2"
     const val logback = "1.2.3"
     const val liquibase = "4.2.2"
