de9d1885bfcdfccd42a8460756969c49f44404d5
Alexey Lapin <alexey-lapin@protonmail.com>
2021-07-07 20:34:45 +0300

    bump versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,11 +1,11 @@
 pluginManagement {
     plugins {
-        id("com.diffplug.spotless") version "5.14.0"
+        id("com.diffplug.spotless") version "5.14.1"
         id("com.github.ben-manes.versions") version "0.39.0"
         id("com.github.johnrengelman.shadow") version "7.0.0"
         id("com.gorylenko.gradle-git-properties") version "2.2.4"
-        id("io.micronaut.application") version "1.5.2"
-        id("io.micronaut.library") version "1.5.2"
+        id("io.micronaut.application") version "2.0.2"
+        id("io.micronaut.library") version "2.0.2"
     }
 }
 
