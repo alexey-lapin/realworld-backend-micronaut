254aa95906b6946c0377430c61b26cad27d12884
Alexey Lapin <alexey-lapin@protonmail.com>
2021-09-20 20:40:11 +0300

    bump versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,7 +4,7 @@ pluginManagement {
     plugins {
         id("com.github.ben-manes.versions") version "0.39.0"
         id("com.github.johnrengelman.shadow") version "7.0.0"
-        id("com.gorylenko.gradle-git-properties") version "2.2.4"
+        id("com.gorylenko.gradle-git-properties") version "2.3.1"
     }
 }
 
