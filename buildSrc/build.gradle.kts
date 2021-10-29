a5ca104d314237852fd82b0c8a0827c8f7d784e3
Alexey Lapin <alexey-lapin@protonmail.com>
2021-10-29 20:07:09 +0300

    bump versions

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,5 +10,5 @@ repositories {
 dependencies {
     implementation("com.bmuschko:gradle-docker-plugin:6.7.0")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:5.14.3")
-    implementation("io.micronaut.gradle:micronaut-gradle-plugin:2.0.3")
+    implementation("io.micronaut.gradle:micronaut-gradle-plugin:2.0.8")
 }
