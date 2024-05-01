f39b737b04a559e680b090e240d62178c8d917de
Alexey Lapin <alexey-lapin@protonmail.com>
2024-04-30 21:15:39 -0400

    add gradle release plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,28 +1,36 @@
 plugins {
-//    id("com.github.ben-manes.versions")
-    id("jacoco-report-aggregation")
+    base
+    alias(libs.plugins.versions)
+    alias(libs.plugins.release)
+    id("jacoco-aggregation")
 }
 
 description = "Real world backend API built in Micronaut"
 
-//dependencies {
-//    implementation(project(":service"))
-//}
+scmVersion {
+    tag {
+        prefix.set("v")
+    }
+}
 
-//tasks {
-//    dependencyUpdates {
-//        checkConstraints = true
-//        resolutionStrategy {
-//            componentSelection {
-//                all {
-//                    val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview", "b", "ea")
-//                        .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-+]*") }
-//                        .any { it.matches(candidate.version) }
-//                    if (rejected) {
-//                        reject("Release candidate")
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+dependencies {
+    implementation(project(":service"))
+}
+
+tasks {
+    dependencyUpdates {
+        checkConstraints = true
+        resolutionStrategy {
+            componentSelection {
+                all {
+                    val rejected = listOf("alpha", "beta", "rc", "cr", "m", "preview", "b", "ea")
+                        .map { qualifier -> Regex("(?i).*[.-]$qualifier[.\\d-+]*") }
+                        .any { it.matches(candidate.version) }
+                    if (rejected) {
+                        reject("Release candidate")
+                    }
+                }
+            }
+        }
+    }
+}
--- gradle/plugins/common/build.gradle.kts
+++ gradle/plugins/common/build.gradle.kts
@@ -11,4 +11,5 @@ dependencies {
     implementation("com.bmuschko:gradle-docker-plugin:6.7.0")
     implementation("com.diffplug.spotless:spotless-plugin-gradle:6.25.0")
     implementation("io.micronaut.gradle:micronaut-gradle-plugin:2.0.8")
+    implementation("pl.allegro.tech.build:axion-release-plugin:1.17.2")
 }
